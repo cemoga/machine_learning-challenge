7b880695cf1b182622b8f16613f1f9887466a498
Cesar <cemoga@yahoo.com>
2020-02-09 02:08:34 -0500

    Final Formatting

--- src/static/predictions.js
+++ src/static/predictions.js
@@ -1,3 +1,17 @@
+$(function () {
+    var current_progress = 0;
+    var interval = setInterval(function () {
+        current_progress += 10;
+        $("#dynamic")
+            .css("width", current_progress + "%")
+            .attr("aria-valuenow", current_progress)
+            .text(current_progress + "% Complete");
+        if (current_progress >= 90)
+            clearInterval(interval);
+    }, 1000);
+});
+
+
 var url = `/predict`;
 
 var model = [],
@@ -43,9 +57,9 @@ d3.json(url).then((data) => {
 
 
             var formatter = new google.visualization.NumberFormat(
-                {suffix: '%',pattern:'#'}
+                { suffix: '%', pattern: '#' }
             );
-                formatter.format(data,1);
+            formatter.format(data, 1);
 
             var options = {
                 min: 0, max: 100,
@@ -81,6 +95,13 @@ d3.json(url).then((data) => {
         var table = new google.visualization.Table(document.getElementById('table_div'));
         table.draw(data, { showRowNumber: true, width: '100%', height: '100%' });
 
+        document.getElementsByClassName('hide')[0].style.visibility = 'hidden';
+        document.getElementsByClassName('show')[0].style.visibility = 'visible';
+        document.getElementsByClassName('show')[0].innerHTML = 'Predictions';
+
+        document.getElementsByClassName('label')[0].innerHTML = prediction[0][0];
+        document.getElementsByClassName('label')[1].innerHTML = prediction[3][0];
+        document.getElementsByClassName('label')[2].innerHTML = prediction[6][0];
     }
 })
 
