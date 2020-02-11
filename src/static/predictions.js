a9e36142434734b0060dcea79b1b8d8105087233
Cesar Mosquera <cemoga@Cesars-MacBook-Air.local>
2020-02-10 19:21:09 -0500

    Javascript commented

--- src/static/predictions.js
+++ src/static/predictions.js
@@ -1,5 +1,7 @@
+// Hide the button to add a new image until the first result is rendered
 document.getElementsByClassName('show')[1].style.visibility = 'hidden';
 
+// Control the progress in the bar in the html code
 $(function () {
     var current_progress = 0;
     var interval = setInterval(function () {
@@ -13,13 +15,17 @@ $(function () {
     }, 1000);
 });
 
+// Flask Route that contains the predicted data from the models
 var url = `/predict`;
 
+// Variable initialization for the information from flask for the predictions
+// and probabilities
 var model = [],
     no = [],
     prediction = [],
     probability = [];
 
+// Create arrays for all the data that comes from flask in JSON format
 d3.json(url).then((data) => {
     // console.log(data)
     for (i = 0; i < 3; i++) {
@@ -31,24 +37,29 @@ d3.json(url).then((data) => {
         })
     }
 
+    // Check the array of information in console
     console.log(model),
         console.log(no),
         console.log(prediction),
         console.log(probability);
 
+    // Google library for Gauges 
     google.charts.load('current', {
         callback: function () {
             var container = document.getElementById('chart_div');
             var chart = new google.visualization.Gauge(container);
 
+            // Convert the probabilities to a float number
             var p1 = parseFloat(probability[0])
             var p2 = parseFloat(probability[3])
             var p3 = parseFloat(probability[6])
 
+            // Verify values of probabilities in console
             console.log(p1)
             console.log(p2)
             console.log(p3)
 
+            // Input the information to the Gauge
             var data = google.visualization.arrayToDataTable([
                 ['Label', 'Value'],
                 ['VGG16', p1],
@@ -56,12 +67,13 @@ d3.json(url).then((data) => {
                 ['RESNET50', p3]
             ]);
 
-
+            // Create percentage format for the probabilities
             var formatter = new google.visualization.NumberFormat(
                 { suffix: '%', pattern: '#' }
             );
             formatter.format(data, 1);
-
+            
+            // Options for the Google Gauge Library
             var options = {
                 min: 0, max: 100,
                 width: 500, height: 255,
@@ -71,16 +83,19 @@ d3.json(url).then((data) => {
                 minorTicks: 10
             };
 
+            // Render the Gauge
             chart.draw(data, options);
         },
         packages: ['gauge']
     });
 
+    // Load the Google Library for Charts
     google.charts.load('current', { 'packages': ['table'] });
     google.charts.setOnLoadCallback(drawTable);
 
 
     function drawTable() {
+        // Set the columns of the chart
         var data = new google.visualization.DataTable();
         data.addColumn('number', 'Probability');
         data.addColumn('string', 'Prediction')
@@ -88,6 +103,7 @@ d3.json(url).then((data) => {
         data.addColumn('number', 'Model Prediction');
         ;
 
+        // Add the columns to the table from the Flask information in JSON format
         for (i = 0; i < 9; i++) {
             data.addRows([
                 [{ v: parseFloat(probability[i]), f: probability[i] }, prediction[i][0], model[i], no[i]]
@@ -98,14 +114,20 @@ d3.json(url).then((data) => {
         // sort by column 0, descending
         data.sort({ column: 0, desc: true });
 
+        // Create the table
         var table = new google.visualization.Table(document.getElementById('table_div'));
         table.draw(data, { showRowNumber: true, width: '100%', height: '100%' });
 
+        // Hide the progress bar once the Gauge and Table are created
         document.getElementsByClassName('hide')[0].style.visibility = 'hidden';
+        // Show the title for the Gauge and Table once the Gauge and Table are created
         document.getElementsByClassName('show')[0].style.visibility = 'visible';
+        // Show the button for a new image once the Gauge and Table are created
         document.getElementsByClassName('show')[1].style.visibility = 'visible';
+        // Create the title for the Gauge and Table once the Gauge and Table are created
         document.getElementsByClassName('show')[0].innerHTML = 'Predictions';
 
+        // Render the information down the Google Gauges that cannot be rendered from the library
         document.getElementsByClassName('label')[0].innerHTML = prediction[0][0];
         document.getElementsByClassName('label')[1].innerHTML = prediction[3][0];
         document.getElementsByClassName('label')[2].innerHTML = prediction[6][0];
